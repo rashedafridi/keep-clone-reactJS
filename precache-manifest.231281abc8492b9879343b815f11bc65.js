3201cd0655f9176bec0ba7b93b32ee59fa77fa3f
Rashed Afridi <rashedafridi@gmail.com>
2021-02-01 02:36:08 +0600

    Updates

---
+++ precache-manifest.231281abc8492b9879343b815f11bc65.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea558e0b2d867f3131bedf1fc1a36b20",
+    "url": "/keep-clone-reactJS/index.html"
+  },
+  {
+    "revision": "937f2036482c4ae12e93",
+    "url": "/keep-clone-reactJS/static/js/2.78bd790b.chunk.js"
+  },
+  {
+    "revision": "9f1f2c50f8c52de65250",
+    "url": "/keep-clone-reactJS/static/js/main.734d2caf.chunk.js"
+  },
+  {
+    "revision": "c779b1346d7cc3ea5c3b",
+    "url": "/keep-clone-reactJS/static/js/runtime-main.548f62a1.js"
+  }
+]);
